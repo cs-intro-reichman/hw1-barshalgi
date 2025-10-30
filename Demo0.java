e8c6f55ff411debaf28813a3d44b045a225603f9
barshalgi <bar.shalgi@post.runi.ac.il>
2025-10-30 10:54:33 +0200

    Update Demo0.java

--- Demo0.java
+++ Demo0.java
@@ -4,11 +4,11 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
-		while (i < 6);
+		while (i < 6);{
 		// print i and add 1
 		System.out.println(i);
 		i = + 1;
-
+		}
         // Complete the program's code here:
     } System.out.println("Done");
  }
